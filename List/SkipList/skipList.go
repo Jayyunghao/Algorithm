e754341bdd39a96ae90924785bc43e8947c6524a
suoyudong <suoyudong@jd.com>
2021-12-15 13:44:51 +0800

    feat(skipList): 增加跳表

--- List/SkipList/skipList.go
+++ List/SkipList/skipList.go
@@ -4,3 +4,38 @@ package SkipList
 //应用实例:Redis有序集合(sort set)
 //时间复杂度: O(m*logn),其中m=3,查询任意时间复杂度为O(logn)
 
+// @lc code=start
+type Skiplist struct {
+
+}
+
+
+func Constructor() Skiplist {
+
+}
+
+
+func (this *Skiplist) Search(target int) bool {
+
+}
+
+
+func (this *Skiplist) Add(num int)  {
+
+}
+
+
+func (this *Skiplist) Erase(num int) bool {
+
+}
+
+
+/**
+ * Your Skiplist object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Search(target);
+ * obj.Add(num);
+ * param_3 := obj.Erase(num);
+ */
+// @lc code=end
+
